0626fa8c95c10dc9fde5271bc9ea61967dfd6117
Thomas Castonguay-Gagnon <castonguay-gagnon.thomas@courrier.uqam.ca>
2023-08-29 11:24:18 -0400

    Update list.php

--- list.php
+++ list.php
@@ -146,7 +146,7 @@ displayNav ( 'list.php' );
 		</table>
 	</div>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="/vendor/js/jquery-2.1.4.min.js"></script>
+	<script src="/vendor/js/jquery.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="/vendor/js/bootstrap.min.js"></script>
 </body>
